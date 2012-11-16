81f92938f187fab7e9b1f7f6b21bfbed8123833e
Andre Richards <andrev.richards@gmail.com>
2012-11-16 14:25:29 -0500

    Fix classnames to use proper capitalization

--- tutorial.js
+++ tutorial.js
@@ -1,5 +1,5 @@
 /**
- * tutorial.js
+ * Tutorial.js
  * Author: Andre Richards
  * Create fast, interactive application tutorials
  * November, 2012
@@ -10,16 +10,16 @@
  *
  * Instructions:
  * To the bottom of your webpage, open new javascript tag and 
- * use tutorial.setup(array) with an array of ids, to prepare 
+ * use Tutorial.setup(array) with an array of ids, to prepare 
  * the tutorial.
  *
- * Call tutorial.start() when ready to begin the tutorial.
+ * Call Tutorial.start() when ready to begin the tutorial.
  *
- * Note: tutorial.js only works with dom element ids.
+ * Note: Tutorial.js only works with dom element ids.
  */
 
 
-function tutorial()
+function Tutorial()
 {
 	this.elementList        = new Array;
 	this.position           = 0;
@@ -40,82 +40,82 @@ function tutorial()
 	this.current_item;
 	this.animation_speed;
 
-	tutorial.prototype.create = function()
+	Tutorial.prototype.create = function()
 	{
 		var elem = document.createElement('div');
 		elem.id = 'tutorial';
 		document.body.appendChild(elem);
 
-		$(tutorial.container).css({
+		$(Tutorial.container).css({
 			'position':'fixed',
 			'height':'100%',
 			'width':'100%',
-			'z-index':tutorial.zindex,
+			'z-index':Tutorial.zindex,
 			'top': 0,
 			'left': 0,
 			'display':'none'
 		});
 
 		// create overlay
-		$(tutorial.container).append("<div id='tutorial-overlay'></div>");
-		tutorial.setup_overlay();
+		$(Tutorial.container).append("<div id='tutorial-overlay'></div>");
+		Tutorial.setup_overlay();
 
 		// create items container.
-		$(tutorial.container).append("<div id='tutorial-items'></div>");
+		$(Tutorial.container).append("<div id='tutorial-items'></div>");
 	}
 
-	tutorial.prototype.start = function()
+	Tutorial.prototype.start = function()
 	{
-		tutorial.position = -1;
-		$(tutorial.container).fadeIn(tutorial.animation_speed);
-		tutorial.disable_scroll();
-		if(tutorial.lengthOfTutorial > 0)
-			tutorial.next();
+		Tutorial.position = -1;
+		$(Tutorial.container).fadeIn(Tutorial.animation_speed);
+		Tutorial.disable_scroll();
+		if(Tutorial.lengthOfTutorial > 0)
+			Tutorial.next();
 	}
 
-	tutorial.prototype.end = function()
+	Tutorial.prototype.end = function()
 	{
-		tutorial.current_item.destroy();
-		tutorial.destroy();
+		Tutorial.current_item.destroy();
+		Tutorial.destroy();
 		$('html, body').animate({
 				scrollTop:0
 		});
-		tutorial.enable_scroll();
+		Tutorial.enable_scroll();
 
 	}
 
-	tutorial.prototype.setup = function(items)
+	Tutorial.prototype.setup = function(items)
 	{
-		tutorial.create();
-		tutorial.lengthOfTutorial = items.length;
-		tutorial.elementList = items;
-		tutorial.animation_speed = Math.floor(this.transition_speed/2);
+		Tutorial.create();
+		Tutorial.lengthOfTutorial = items.length;
+		Tutorial.elementList = items;
+		Tutorial.animation_speed = Math.floor(this.transition_speed/2);
 	}
 
 	// prep the overlay with configurations.
-	tutorial.prototype.setup_overlay = function()
+	Tutorial.prototype.setup_overlay = function()
 	{       
-		$(tutorial.overlay).css({
+		$(Tutorial.overlay).css({
 			'position':'absolute',
 			'height':'100%',
 			'width':'100%',
-			'z-index':tutorial.overlay_zindex,
-			'background-color': tutorial.overlay_color,
-			'opacity': tutorial.overlay_opacity,
+			'z-index':Tutorial.overlay_zindex,
+			'background-color': Tutorial.overlay_color,
+			'opacity': Tutorial.overlay_opacity,
 			'top': 0,
 			'left': 0
 		});
 	}
 
-	tutorial.prototype.show = function(position)
+	Tutorial.prototype.show = function(position)
 	{
 		// create container for item
-		$(tutorial.items).append('<div class="tutorial-item-container" id="tutorial-item-container"></div>');
-		var item_container = $(tutorial.item_container);
+		$(Tutorial.items).append('<div class="tutorial-item-container" id="tutorial-item-container"></div>');
+		var item_container = $(Tutorial.item_container);
 
 		// create item for container.
-		elem = tutorial.elementList[position];
-		var item = new tutorial_item(elem);
+		elem = Tutorial.elementList[position];
+		var item = new Tutorial_item(elem);
 
 		// insert into container.
 		$(item_container).append(item.element);
@@ -135,36 +135,36 @@ function tutorial()
 		// setup clicktrap
 		item.trap();
 
-		$(item_container).fadeIn(tutorial.animation_speed);
+		$(item_container).fadeIn(Tutorial.animation_speed);
 
-		tutorial.current_item = item;
+		Tutorial.current_item = item;
 	}
 
-	tutorial.prototype.hide = function()
+	Tutorial.prototype.hide = function()
 	{
-		if(tutorial.current_item != null)
-		tutorial.current_item.destroy();
+		if(Tutorial.current_item != null)
+		Tutorial.current_item.destroy();
 	}
 
-	tutorial.prototype.next = function()
+	Tutorial.prototype.next = function()
 	{
-		next = tutorial.position+1;
-		if(next < tutorial.lengthOfTutorial){
-			tutorial.hide();
-			tutorial.scroll_to(tutorial.elementList[next]);
-			setTimeout(function(){tutorial.show(next);}, tutorial.animation_speed+tutorial.scroll_speed+100);
-			tutorial.position = next;
+		next = Tutorial.position+1;
+		if(next < Tutorial.lengthOfTutorial){
+			Tutorial.hide();
+			Tutorial.scroll_to(Tutorial.elementList[next]);
+			setTimeout(function(){Tutorial.show(next);}, Tutorial.animation_speed+Tutorial.scroll_speed+100);
+			Tutorial.position = next;
 		}
 		else
-			tutorial.end();
+			Tutorial.end();
 	}
 
-	tutorial.prototype.previous = function()
+	Tutorial.prototype.previous = function()
 	{
 		
 	}
 
-	tutorial.prototype.scroll_to = function(elem)
+	Tutorial.prototype.scroll_to = function(elem)
 	{
 		var item = $("#"+elem);
 		if(item.length>0){
@@ -174,19 +174,19 @@ function tutorial()
 			var itemHeight = $(item).height();
 			var itemTop = $(item).offset().top;
 
-			if(tutorial.alternate_scroll == true)
+			if(Tutorial.alternate_scroll == true)
 				scroll_value = (itemTop+itemHeight)-(windowHeight/2)-100;
 			else
 				scroll_value = itemTop-scrollTop-itemHeight;
 
 			$('html, body').animate({
 				scrollTop:scroll_value
-			}, tutorial.scroll_speed);
+			}, Tutorial.scroll_speed);
 		}
 		return true;
 	}
 
-	tutorial.prototype.disable_scroll = function()
+	Tutorial.prototype.disable_scroll = function()
 	{
 		$("body").css("overflow","hidden");
 		$(document).bind('mousewheel DOMMouseScroll',function(e){ 
@@ -200,20 +200,20 @@ function tutorial()
 	});
 	}
 
-	tutorial.prototype.enable_scroll = function()
+	Tutorial.prototype.enable_scroll = function()
 	{
 		$("body").css("overflow","auto");
 		$(document).unbind('mousewheel DOMMouseScroll');
 	}
 
-	tutorial.prototype.destroy = function()
+	Tutorial.prototype.destroy = function()
 	{
-		elem = $(tutorial.container);
-		elem.fadeOut(tutorial.animation_speed, function(){$(tutorial.items).html()});
+		elem = $(Tutorial.container);
+		elem.fadeOut(Tutorial.animation_speed, function(){$(Tutorial.items).html()});
 	}
 }
 
-function tutorial_item(id)
+function Tutorial_item(id)
 {
 	this.width;
 	this.height;
@@ -242,10 +242,10 @@ function tutorial_item(id)
 	this.element = clone; 
 
 	// add clicktrap to cloned item.
-	tutorial_item.prototype.trap = function()
+	Tutorial_item.prototype.trap = function()
 	{
 		$(this.element).append('<div id="tutorial-clicktrap"></div>');
-		$(tutorial.clicktrap).css({
+		$(Tutorial.clicktrap).css({
 			'position':'absolute',
 			'height':'100%',
 			'width':'100%',
@@ -254,8 +254,8 @@ function tutorial_item(id)
 			'cursor':'pointer',         
 		});
 
-		if(tutorial.item_outline == true)
-			$(tutorial.clicktrap).css({
+		if(Tutorial.item_outline == true)
+			$(Tutorial.clicktrap).css({
 				'padding':'10px',
 				'margin': '-13px',
 				'border': 'dashed 3px #ffF'
@@ -263,12 +263,12 @@ function tutorial_item(id)
 	}
 
 	// destroy
-	tutorial_item.prototype.destroy = function()
+	Tutorial_item.prototype.destroy = function()
 	{
 		elem = this.element;
-		elem.fadeOut(tutorial.animation_speed, function(){$(tutorial.item_container).remove()});
+		elem.fadeOut(Tutorial.animation_speed, function(){$(Tutorial.item_container).remove()});
 	}
 }
 
-var tutorial = new tutorial;
-$(document).on('click',tutorial.clicktrap,function(e){e.preventDefault();tutorial.next()});
+var Tutorial = new Tutorial;
+$(document).on('click',Tutorial.clicktrap,function(e){e.preventDefault();Tutorial.next()});
